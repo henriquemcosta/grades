f4d53459a1da0bd07134463bea0afca64fbab54d
henriquemcosta <henriquemcosta@gmail.com>
2020-11-09 21:57:52 -0300

    Correção do GET /grade tirado uma barra

--- routes/gradeRouter.js
+++ routes/gradeRouter.js
@@ -4,7 +4,7 @@ import controller from '../controllers/gradeController.js';
 const app = express();
 
 app.post('/grade/', controller.create);
-app.get('/grade/', controller.findAll);
+app.get('/grade', controller.findAll);
 app.get('/grade/:id', controller.findOne);
 app.put('/grade/:id', controller.update);
 app.delete('/grade/:id', controller.remove);
