7f24b611d1d7ebc9a145e581cfb8b27ba0756b2a
henriquemcosta <henriquemcosta@gmail.com>
2020-11-09 22:00:09 -0300

    cors tirando barra

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://my-grade-app.herokuapp.com/',
+    origin: 'https://my-grade-app.herokuapp.com',
   })
 );
 app.use('/', gradeRouter);
